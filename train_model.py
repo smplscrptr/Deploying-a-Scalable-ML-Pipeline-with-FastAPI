c874cb5f84ec23aff41e6bd4c82a00b55fa19a9a
Bennett Woods <bwoo175@wgu.edu>
2025-06-29 11:04:55 -0500

    FIX: File path

--- train_model.py
+++ train_model.py
@@ -13,7 +13,7 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "/Users/bennett/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/data/census.csv"
+project_path = "/Users/bennett/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path)
